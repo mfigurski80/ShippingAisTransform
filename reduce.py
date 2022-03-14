c09c3d374e41107a2086436c192f78881e4f78b9
Mikolaj Figurski <meek.f80@gmail.com>
2022-03-14 10:42:06 -0400

    List reduce

--- reduce.py
+++ reduce.py
@@ -6,19 +6,18 @@ from forEach import forEachDataset, getFromZip
 def findUniqueShips(in_f, out_w):
     r = csv.reader(in_f)
     uniq = set({})
-    to_pull = np.array([0, 7, 10, 12, 13, 15])
-    headers = np.array(next(r))
-    out_w.writerow(headers[to_pull])
+    headers = next(r)
+    out_w.writerow(
+        (headers[0], headers[7], headers[10], headers[12], headers[13], headers[15])
+    )
     for row in r:
         if int("0" + row[10]) > 70:  # if cargo or tanker...
-            row = np.array(row)
             if row[0] not in uniq:
                 uniq.add(row[0])
-                out_w.writerow(row[to_pull])
+                out_w.writerow((row[0], row[7], row[10], row[12], row[13], row[15]))
 
 
 if __name__ == "__main__":
-    print("HIIII")
     #  getFromZip(
     #  "https://coast.noaa.gov/htdata/CMSP/AISDataHandler/2018/AIS_2018_01_01.zip"
     #  )
