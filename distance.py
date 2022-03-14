abd5ec4d9713195de24d74eaba469e7625b4c37a
Mikolaj Figurski <meek.f80@gmail.com>
2022-03-14 12:00:39 -0400

    Add haversine formula for distance between long/lat

---
+++ distance.py
@@ -0,0 +1,17 @@
+from math import radians, cos, sin, asin, sqrt
+
+RADIUS = 6371  # of earth, in km
+
+
+def distance(lat1, lon1, lat2, lon2):
+    lat1 = radians(lat1)
+    lat2 = radians(lat2)
+    lon1 = radians(lon1)
+    lon2 = radians(lon2)
+
+    # Haversine Formula
+    dlon = lon2 - lon1
+    dlat = lat2 - lat1
+    a = sin(dlat / 2) ** 2 + cos(lat1) * cos(lat2) * sin(dlon / 2) ** 2
+    c = 2 * asin(sqrt(a))
+    return c * RADIUS
